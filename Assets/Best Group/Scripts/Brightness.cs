cc752258006edd51a2e0cbbf469e374c77b1b8d2
Niklas Blomqvist <ntjblomqvist@gmail.com>
2015-12-04 13:56:46 +0100

    Brightness-slider added with keys "O" and "L"

---
+++ Assets/Best Group/Scripts/Brightness.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brightness : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.O))
+        {
+            RenderSettings.ambientIntensity += 0.01f;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            RenderSettings.ambientIntensity -= 0.01f;
+        }
+    }
+}
